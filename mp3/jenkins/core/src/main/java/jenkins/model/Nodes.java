8d5981813d473e79e4fcc71920ee31352b037cb7
unknown <Bradley.Tyler@uky.edu>
2020-04-01 16:01:37 -0400

    q3

--- mp3/jenkins/core/src/main/java/jenkins/model/Nodes.java
+++ mp3/jenkins/core/src/main/java/jenkins/model/Nodes.java
@@ -112,7 +112,7 @@ public class Nodes implements Saveable {
                     Nodes.this.nodes.put(name, n);
                 }
                 Nodes.this.nodes.keySet().removeAll(toRemove); // directory clean up will be handled by save
-                updateAndTrim();
+                jenkins.updateAndTrim();
             }
         });
         save();
@@ -132,7 +132,7 @@ public class Nodes implements Saveable {
                 @Override
                 public void run() {
                     nodes.put(node.getNodeName(), node);
-                    updateAndTrim();
+                    jenkins.updateAndTrim();
                 }
             });
             // no need for a full save() so we just do the minimum
@@ -164,7 +164,7 @@ public class Nodes implements Saveable {
                         c.disconnect(OfflineCause.create(hudson.model.Messages._Hudson_NodeBeingRemoved()));
                     }
                     if (node == nodes.remove(node.getNodeName())) {
-                        updateAndTrim();
+                        jenkins.updateAndTrim();
                     }
                 }
             });
@@ -248,7 +248,7 @@ public class Nodes implements Saveable {
                     }
                 }
                 nodes.putAll(newNodes);
-                updateAndTrim();
+                jenkins.updateAndTrim();
             }
         });
     }
@@ -275,9 +275,4 @@ public class Nodes implements Saveable {
     public boolean isLegacy() {
         return !new File(jenkins.getRootDir(), "nodes").isDirectory();
     }
-
-	private void updateAndTrim() {
-		jenkins.updateComputerList();
-		jenkins.trimLabels();
-	}
 }
