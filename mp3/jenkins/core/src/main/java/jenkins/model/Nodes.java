7f52362951080c7068b0ca267300b8cd67fd1471
unknown <Bradley.Tyler@uky.edu>
2020-04-01 15:19:58 -0400

    q2

--- mp3/jenkins/core/src/main/java/jenkins/model/Nodes.java
+++ mp3/jenkins/core/src/main/java/jenkins/model/Nodes.java
@@ -112,8 +112,7 @@ public class Nodes implements Saveable {
                     Nodes.this.nodes.put(name, n);
                 }
                 Nodes.this.nodes.keySet().removeAll(toRemove); // directory clean up will be handled by save
-                jenkins.updateComputerList();
-                jenkins.trimLabels();
+                updateAndTrim();
             }
         });
         save();
@@ -133,8 +132,7 @@ public class Nodes implements Saveable {
                 @Override
                 public void run() {
                     nodes.put(node.getNodeName(), node);
-                    jenkins.updateComputerList();
-                    jenkins.trimLabels();
+                    updateAndTrim();
                 }
             });
             // no need for a full save() so we just do the minimum
@@ -166,8 +164,7 @@ public class Nodes implements Saveable {
                         c.disconnect(OfflineCause.create(hudson.model.Messages._Hudson_NodeBeingRemoved()));
                     }
                     if (node == nodes.remove(node.getNodeName())) {
-                        jenkins.updateComputerList();
-                        jenkins.trimLabels();
+                        updateAndTrim();
                     }
                 }
             });
@@ -251,8 +248,7 @@ public class Nodes implements Saveable {
                     }
                 }
                 nodes.putAll(newNodes);
-                jenkins.updateComputerList();
-                jenkins.trimLabels();
+                updateAndTrim();
             }
         });
     }
@@ -279,4 +275,9 @@ public class Nodes implements Saveable {
     public boolean isLegacy() {
         return !new File(jenkins.getRootDir(), "nodes").isDirectory();
     }
+
+	private void updateAndTrim() {
+		jenkins.updateComputerList();
+		jenkins.trimLabels();
+	}
 }
